6068d21ed99d3bffc152d914d418455374ebff5e
allhelp <42856781+allhelp@users.noreply.github.com>
2018-09-23 19:51:05 +0800

    Add files via upload

---
+++ kff/add.php
@@ -0,0 +1,37 @@
+﻿<?php
+
+/**
+
+ * @功能 将表单插入数据库
+
+ */
+
+$content=$_POST['content'];
+
+print_r($_POST);
+
+$time= date("Y-m-d H:i:s", time(0));
+
+$link = mysqli_connect('localhost', 'root', '123', 'test');
+
+mysqli_query($link, 'set names utf8');
+
+$ip = $_SERVER["REMOTE_ADDR"];
+
+$sql="INSERT INTO talk VALUES(NULL,'匿名者','$ip','$content','$time')";
+
+$data = mysqli_query($link,$sql);
+
+echo "$content";
+
+if($data)
+
+ echo "1";
+
+ else
+
+  echo "0";
+
+---------------------
+
+本文来自 Lz18310851892 的CSDN 博客 ，全文地址请点击：https://blog.csdn.net/qq_40842615/article/details/80671343?utm_source=copy 
---
+++ kff/chat.sql
@@ -0,0 +1,21 @@
+﻿DROP TABLE IF EXISTS `talk`;
+
+CREATE TABLE `talk` (
+
+ `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'id',
+
+ `send` varchar(10) CHARACTER SET utf8 DEFAULT NULL COMMENT '发送者昵称',
+
+ `ip` varchar(12) CHARACTER SET utf8 DEFAULT NULL,
+
+ `content` varchar(500) CHARACTER SET utf8 DEFAULT NULL,
+
+ `time` varchar(50) DEFAULT NULL,
+
+ PRIMARY KEY (`id`)
+
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
+
+---------------------
+
+本文来自 Lz18310851892 的CSDN 博客 ，全文地址请点击：https://blog.csdn.net/qq_40842615/article/details/80671343?utm_source=copy 
